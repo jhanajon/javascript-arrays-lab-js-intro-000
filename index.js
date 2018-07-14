7868d87382f03fb37098abea733cca97d5bd5104
joe fisher <jhanajon@gmail.com>
2018-07-14 22:04:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Add your functions and code here
+function destructivelyAppendKitten(array,name) {
+  var kittens = ['Milo', 'Otis', 'Garfield']
+   array = kittens
+  array = array.push(name)
+  //array = Array.push(name)
+  // return(array)
+}
